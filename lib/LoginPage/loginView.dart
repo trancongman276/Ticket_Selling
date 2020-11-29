ef52f0987d2f604cc52ed69f0c66de1211dc61a9
Doku <trancongman276@gmail.com>
2020-11-29 13:56:39 +0700

    Updated Login

--- lib/LoginPage/loginView.dart
+++ lib/LoginPage/loginView.dart
@@ -5,14 +5,12 @@ import 'Views/top_view.dart';
 
 
 class LoginView extends StatelessWidget {
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Stack(
-        children: <Widget>[TopView(), BodyView(formkey: _formKey,), BottomView()],
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[TopView(), BodyView(), BottomView()],
       ),
     );
   }
---
+++ lib/MainPage/UserMainView.dart
@@ -0,0 +1,78 @@
+import 'dart:io';
+
+import 'package:CoachTicketSelling/Utils/GlobalValues.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class UserMainView extends StatefulWidget {
+  final String id;
+
+  const UserMainView({Key key, @required this.id}) : super(key: key);
+  @override
+  _UserMainViewState createState() => _UserMainViewState(this.id);
+}
+
+class _UserMainViewState extends State<UserMainView> {
+  final uid;
+  _UserMainViewState(this.uid);
+
+  @override
+  Widget build(BuildContext context) {
+    final User user = Utils.firebaseAuth.currentUser;
+
+    Future<bool> _onWillPop() {
+      return showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+                title: Text('Exit'),
+                content: Text('Do you want to close app?'),
+                actions: <Widget>[
+                  FlatButton(
+                      onPressed: () => Navigator.of(context).pop(false),
+                      child: Text('No')),
+                  FlatButton(onPressed: () => exit(0), child: Text('Yes')),
+                ],
+              ));
+    }
+
+    return WillPopScope(
+      onWillPop: () => _onWillPop(),
+      child: Scaffold(
+        body: SafeArea(
+          child: ListView(
+            padding: EdgeInsets.symmetric(vertical: 10.0),
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Hello ${user.displayName.split(' ')[0]},",
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  IconButton(
+                    icon: FaIcon(FontAwesomeIcons.userCircle),
+                    onPressed: () =>
+                        {Utils.logout(), Navigator.of(context).pop()},
+                        // TODO: 
+                        
+                  ),
+                ],
+              ),
+              Text(
+                "Where do you want to go today?",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontStyle: FontStyle.italic,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
