796c3a548a1b9fc741c54567b065106ef8112766
Doku <trancongman276@gmail.com>
2020-11-30 21:00:08 +0700

    Runnable web

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -9,7 +10,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   print("[DEBUG]Checking login");
-  logined = await route.checkLogined();
+  if (!kIsWeb) logined = await route.checkLogined();
   print("[DEBUG]Checked: $logined");
   runApp(MyApp());
 }
