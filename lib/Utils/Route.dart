f0b3b390daefb3a7cb22edab2e4909c5a900d85e
Doku <trancongman276@gmail.com>
2020-11-30 19:30:17 +0700

    Revert "UI"
    This reverts commit df1fbd4787aa0960bf7d9a1e2c646a4beb81926b.

--- lib/Utils/Route.dart
+++ lib/Utils/Route.dart
@@ -1,7 +1,7 @@
 import 'package:CoachTicketSelling/LoginPage/forgetView.dart';
 import 'package:CoachTicketSelling/LoginPage/loginView.dart';
 import 'package:CoachTicketSelling/LoginPage/registerView.dart';
-//import 'package:CoachTicketSelling/MainPage/Manager/MainView.dart';
+import 'package:CoachTicketSelling/MainPage/Manager/MainView.dart';
 import 'package:CoachTicketSelling/MainPage/UserMainView.dart';
 import 'package:CoachTicketSelling/Utils/GlobalValues.dart';
 import 'package:flutter/material.dart';
@@ -21,8 +21,8 @@ Route<dynamic> generateRoute(RouteSettings settings) {
       return MaterialPageRoute(builder: (context) => RegisterView());
     case ForgetViewRoute:
       return MaterialPageRoute(builder: (context) => ForgetPasswordView());
-    //case ManagerViewRoute:
-      //return MaterialPageRoute(builder: (context) => ManagerMainView());
+    case ManagerViewRoute:
+      return MaterialPageRoute(builder: (context) => ManagerMainView());
     case UserViewRoute:
       var _id = settings.arguments;
       return MaterialPageRoute(builder: (context) => UserMainView(id: _id));
